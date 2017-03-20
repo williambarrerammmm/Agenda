f5f0dbdb8f3d71772e72b2ed30aed4bfda000bfa
William <williambarreramuoz@gmail.com>
2017-03-20 15:17:42 -0500

    Se agrega la clase amigo

---
+++ amigo.py
@@ -0,0 +1,24 @@
+
+
+class amigo:
+	
+	def __init__(self,nombre,apellido,telefonos):
+		self.nombre=nombre
+		self.apellido=apellido
+		self.telefonos=telefonos             #constructor 
+		
+	def agregarTelefono(self,telefono):
+		self.telefonos.append(telefono)
+		
+	def agregarNombre(self,nombrea):
+		self.nombre.append(nombrea)
+		
+	def agregarApellido(self,apellidoa):
+		self.apellido.append(apellidoa)
+
+	def nombres(self):
+		return self.nombre
+	
+	def imprimirAmigo(self):
+		print(self.nombre+" "+self.apellido+" ")
+		print(self.telefonos)
